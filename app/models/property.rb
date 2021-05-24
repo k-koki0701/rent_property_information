8ae1cbeae3fc108d1971fb0c3beb9f755d3562c3
koki kanasaka <k.koki980701@gmail.com>
2021-05-24 16:40:11 +0900

    からの場合は無視

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
   has_many :closest_stations
-  accepts_nested_attributes_for :closest_stations
+  accepts_nested_attributes_for :closest_stations, reject_if: :all_blank
 end
