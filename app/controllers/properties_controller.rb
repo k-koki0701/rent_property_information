cf23b441c50790abb4b5d81938c0d49e15a2564b
koki kanasaka <k.koki980701@gmail.com>
2021-05-25 15:46:15 +0900

    子レコード保存できるようにループ作成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -13,7 +13,7 @@ class PropertiesController < ApplicationController
   # GET /properties/new
   def new
     @property = Property.new
-    @property.closest_stations.build
+    2.times { @property.closest_stations.build }
   end
 
   # GET /properties/1/edit
@@ -65,6 +65,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:property_name, :rent, :adress, :building_age, :note, closest_stations_attributes:[:id,:property_id])
+      params.require(:property).permit(:property_name, :rent, :adress, :building_age, :note, closest_stations_attributes:[:id, :route_name, :station_name, :minites_walk, :property_id, :_destroy])
     end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
   has_many :closest_stations, dependent: :destroy
-  accepts_nested_attributes_for :closest_stations, reject_if: :all_blank
+  accepts_nested_attributes_for :closest_stations, reject_if: :all_blank, allow_destroy: true
 end
